f33cbdbeb1688690ddb1d7cc07dd3af78a637f82
dimaturchyn <dim4ukt@gmail.com>
2022-02-19 13:24:14 +0200

    some changes

--- src/calculator.java
+++ src/calculator.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
 
 public class calculator {
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("Enter first number");
+        double firstnumber  = scaner.nextDouble();
+        System.out.println("Enter second number");
+        double secondnumber  = scaner.nextDouble();
+        System.out.println(firstnumber + secondnumber);
+        System.out.println(firstnumber - secondnumber);
+        System.out.println(firstnumber * secondnumber);
+        System.out.println(firstnumber / secondnumber);
+    }
 
 }
