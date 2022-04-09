f8dfc28b1a5a971ee82b80890164cd74d740cf33
dimaturchyn <dim4ukt@gmail.com>
2022-04-09 10:51:36 +0200

    some changes

--- src/calculator.java
+++ src/calculator.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         Scanner scaner = new Scanner(System.in);
-        System.out.println("Enter first number");
-        double firstnumber  = scaner.nextDouble();
-        System.out.println("Enter second number");
-        double secondnumber  = scaner.nextDouble();
-        System.out.println(firstnumber + secondnumber);
-        System.out.println(firstnumber - secondnumber);
-        System.out.println(firstnumber * secondnumber);
-        System.out.println(firstnumber / secondnumber);
+        System.out.println("your name:");
+        String name = scaner.nextLine();
+        System.out.println("your age:");
+        Integer age = scaner.nextInt();
+        System.out.println("Hello your name is : "+name);
+        System.out.println("Hello your age is : "+age);
     }
 
 }
---
+++ src/ls4.java
@@ -0,0 +1,2 @@
+public class ls4 {
+}
